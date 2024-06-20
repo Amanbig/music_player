0efca9f4ffc5dacf4ee13ae3da8e10eae9eade21
aman <amanpreetsinghjhiwant@outlook.com>
2024-06-20 21:37:57 +0530

    created app

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src/App';
 import {name as appName} from './app.json';
+import TrackPlayer from 'react-native-track-player';
+import { playBackService } from './musicPlayerService';
 
 AppRegistry.registerComponent(appName, () => App);
+TrackPlayer.registerPlaybackService(() => playBackService);
